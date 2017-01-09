188f571477262e7d490315ebed09de614bf60a21
Arvin Wilderink <arvinwilderink@me.com>
2017-01-09 15:50:05 +0100

    Initial Commit

---
+++ Beersume/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Beersume
+//
+//  Created by Arvin Wilderink on 09-01-17.
+//  Copyright © 2017 Arvin Wilderink. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
